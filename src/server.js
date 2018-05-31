f1e09b66a31e58631d553c439d821b18c76c9b48
Katherine Bernardez <katherine.o.bernardez.20@dartmouth.edu>
2018-05-31 00:14:19 -0400

    make it better

--- src/server.js
+++ src/server.js
@@ -3,9 +3,112 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import botkit from 'botkit';
+import dotenv from 'dotenv';
+import yelp from 'yelp-fusion';
+
+dotenv.config({ silent: true });
 
 // initialize
 const app = express();
+
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
+
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+  // this grabs the slack token we exported earlier
+}).startRTM((err) => {
+  // start the real time message client
+  if (err) { throw new Error(err); }
+});
+
+// prepare webhook
+// for now we won't use this but feel free to look up slack webhooks
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) { throw new Error(err); }
+  });
+});
+
+// example hello response
+controller.hears(['hello', 'hi', 'howdy'], ['direct_message'], (bot, message) => {
+  // bot.reply(message, 'Hello there!');
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.reply(message, `Hello, ${res.user.name}!`);
+    } else {
+      bot.reply(message, 'Hello there!');
+    }
+  });
+});
+
+// controller.on('user_typing', (bot, message) => {
+//   bot.reply(message, 'stop typing!');
+// });
+let userType = '';
+let userLocation = '';
+
+controller.hears(['hungry'], ['ambient', 'direct_message'], (bot, message) => {
+  bot.startConversation(message, askFood);
+});
+
+function askFood(response, convo) {
+  convo.ask('Would you like food recomendations near you?', (response, convo) => {
+    convo.say('Great!');
+    askType(response, convo);
+    convo.next();
+  });
+}
+function askType(response, convo) {
+  convo.ask('What type of food are you interested in?', (response, convo) => {
+    userType = response.text;
+    convo.say('Ok.');
+    askWhere(response, convo);
+    convo.next();
+  });
+}
+function askWhere(response, convo) {
+  convo.ask('Where are you?', (response, convo) => {
+    userLocation = response.text;
+    convo.say('Ok! One sec. Pulling up results.');
+    results(userType, userLocation, convo);
+    convo.next();
+  });
+}
+function results(type, location, convo) {
+  const client = yelp.client(process.env.YELP_CLIENT_SECRET);
+  client.search({
+    term: type,
+    location,
+  }).then((response) => {
+    response.jsonBody.businesses.forEach((business) => {
+      const attachments = {
+        attachments: [
+          {
+            fallback: 'No results.',
+            pretext: `rating: ${business.rating}`,
+            title: business.name,
+            title_link: business.url,
+            image_url: business.image_url,
+          },
+        ],
+      };
+      convo.say(attachments);
+      convo.next();
+    });
+  }).catch((e) => {
+    convo.say('No results.');
+    convo.next();
+  });
+}
+
+controller.on('outgoing_webhook', (bot, message) => {
+  bot.replyPublic(message, 'yeah yeah');
+});
 
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
