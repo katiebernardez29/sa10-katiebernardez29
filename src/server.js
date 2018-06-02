783dcfd215108c068061cfdab256fc5bcd361b01
Katherine Bernardez <katherine.o.bernardez.20@dartmouth.edu>
2018-06-01 22:30:25 -0400

    ugh

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
 });
 
 // example hello response
-controller.on(['hello', 'hi', 'howdy'], ['direct_mention', 'direct_message'], (bot, message) => {
+controller.hears(['hello', 'hi', 'howdy'], ['direct_mention', 'direct_message'], (bot, message) => {
   // bot.reply(message, 'Hello there!');
   bot.api.users.info({ user: message.user }, (err, res) => {
     if (res) {
@@ -46,7 +46,7 @@ controller.on(['hello', 'hi', 'howdy'], ['direct_mention', 'direct_message'], (b
   });
 });
 
-controller.on('help', ['direct_mention'], (bot, message) => {
+controller.hears('help', ['direct_mention', 'direct_message'], (bot, message) => {
   bot.reply(message, 'If you type hungry, I can give you food recommendations.');
 });
 
@@ -61,7 +61,7 @@ controller.on('direct_message', (bot, message) => {
 let userType = '';
 let userLocation = '';
 
-controller.on('hungry', ['direct_mention', 'direct_message'], (bot, message) => {
+controller.hears('hungry', ['direct_mention', 'direct_message'], (bot, message) => {
   bot.startConversation(message, (err, convo) => {
     convo.ask('Would you like food recomendations near you?', [
       {
